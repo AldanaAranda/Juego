45e6b61e2c6af555ab28b91361c964e525a09f79
Lupin07 <aldanaaranda07@gmail.com>
2023-01-28 21:27:35 -0300

    arreglando js

--- js/js.js
+++ js/js.js
@@ -1,23 +1,28 @@
 var y = Math.floor(Math.random() * 10 + 1);
 var intentos = 1;
+console.log(y)
 
-document.addEventListener("keypress", function (e) {
-    document.getElementById("Adivinar").onclick = function(){
-    var x = document.getElementById("Adivinanza").value = function () {
-        if (e.key === "Enter" && x === y) {
-            alertalert("FELICIDADES, lo adivinaste en " + intentos + " intentos")
-        }
-        else if (x > y) {
-            intentos++;
-            alert("Intenta un número más chico")
-        }
-        else {
-            intentos++;
-            alert("Intenta un número más grande")
-        }
+document.addEventListener("keypress", function(e) {
+    if (e.key === "Enter") {
+        const o = document.querySelector("#enviar");
+        o.click(document.getElementById("enviar").onclick = function () {
+            var x = document.getElementById("respuesta").value;
+            if (x == y) {
+                alert("FELICIDADES! Has adivinado en " + intentos + " intentos");
+            }
+            else if (x > y) {
+                intentos++;
+                alert("Intenta con un número más chico");
+            }
+            else {
+                intentos++;
+                alert("Intenta con un número más grande");
+            }
+        })
+
     }
-}
-
 })
+
+
 
 
